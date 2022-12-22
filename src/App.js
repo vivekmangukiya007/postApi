6337f08733f23d58a7db9f1464f4eb35d1f42733
vivekmangukiya007 <vivekmangukiya456@gmail.com>
2022-12-22 16:38:49 +0530

    add postapi using insert and select

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [data, setData] = useState([]);
+  const btn = () => {
+    axios
+      .post('http://localhost/php/api.php', {
+        name,
+        email,
+        password,
+      })
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+    axios
+      .get('http://localhost/php/api.php')
+      .then(function (response) {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" method="post">
+        <table>
+          <tr>
+            <td>Name:</td>
+            <td>
+              <input
+                type="text"
+                value={name}
+                onChange={e => {
+                  setName(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td>
+              <input
+                type="text"
+                value={email}
+                onChange={e => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>Password:</td>
+            <td>
+              <input
+                type="text"
+                value={password}
+                onChange={e => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              <input type="button" value="submit" onClick={btn} />
+            </td>
+          </tr>
+        </table>
+      </form>
+      <table border="">
+        {data.map(i => {
+          return (
+            <>
+              <tr>
+                <td>{i.id}</td>
+                <td>{i.name}</td>
+                <td>{i.email}</td>
+                <td>{i.password}</td>
+              </tr>
+            </>
+          );
+        })}
+      </table>
     </div>
   );
 }
